5aebc698f3cda9cf895b6fe6d229d8acfa58210b
cec12 <97445622+cec12@users.noreply.github.com>
2024-02-24 10:22:45 +1000

    Починка билда

--- CodeEncryption/CodeEncryption.cpp
+++ CodeEncryption/CodeEncryption.cpp
@@ -2,10 +2,11 @@
 /*
 *	Keep unreferenced data, linker option /OPT:NOREF
 */
+
 #pragma section(".code", execute, read, write)
 #pragma comment(linker, "/MERGE:.text=.code")
 #pragma comment(linker, "/MERGE:.data=.code")
-#pragma comment(linker, "/SECTION:.code, ERW")
+#pragma comment(linker, "/SECTION:.code,ERW")
 
 unsigned char var[] = { 0xBE, 0xBA, 0xFE, 0xCA };
 
@@ -14,6 +15,44 @@ unsigned char var[] = { 0xBE, 0xBA, 0xFE, 0xCA };
 
 void main()
 {
-	// pragma code here
+	// program code here
 	return;
 } /*end main()-----------------------------------------------------*/
+
+//.stub SECTION-----------------------------------------------------
+#pragma section(".stub", execute, read)
+#pragma comment(linker, "/entry:\StubEntry\"")
+#pragma code_seg(".stub");
+//#include <stdio.h>
+
+/*
+ can determine these values via dumpbin.exe then set at compile time
+ can also have cryptor parse PE and set these during encryption
+*/
+
+#define CODE_BASE_ADDRESS	0x00401000
+#define CODE_SIZE			0x00000200
+#define KEY					0x0F
+
+void decryprCodeSection()
+{
+	//we'll use a Mickey-Mouse encoding scheme to keep things simple
+	unsigned char* ptr;
+	long int i;
+	long int nbytes;
+	ptr = (unsigned char*)CODE_BASE_ADDRESS;
+	nbytes = CODE_SIZE;
+	for (i = 0; i < nbytes; i++)
+	{
+		ptr[i] = ptr[i] ^ KEY;
+	}
+	return;
+}/*end decryptSection()----------------------------------------------*/
+
+void StubEntry()
+{
+	decryprCodeSection();
+	//printf("Started In Stub()\n");
+	main();
+	return;
+}/*end StubEntry()--------------------------------------------------*/
