e8bbef542d897832e5fb3df54e0131e41b3a0152
cec12 <97445622+cec12@users.noreply.github.com>
2024-02-25 18:35:57 +1000

    Добавлена функция шифрования, убраны неиспользуемые переменные и функции

--- Encryptor/Encryptor.cpp
+++ Encryptor/Encryptor.cpp
@@ -9,35 +9,7 @@ typedef struct _ADDRESS_INFO
 	DWORD fileCodeSize;			//# of bytes used by .code section in file
 }ADDRESS_INFO, * PADDRESS_INFO;
 
-void GetCodeLoc(LPVOID baseAddress, PADDRESS_INFO addrInfo)
-{
-	PIMAGE_DOS_HEADER dosHeader;
-	PIMAGE_NT_HEADERS peHeader;
-	IMAGE_OPTIONAL_HEADER32 optionalHeader;
-
-	dosHeader = (PIMAGE_DOS_HEADER)baseAddress;
-	peHeader = (PIMAGE_NT_HEADERS)((DWORD)baseAddress + (*dosHeader).e_lfanew);
-	optionalHeader = (*peHeader).OptionalHeader;
-
-	(*addrInfo).moduleBase = optionalHeader.ImageBase;
-	(*addrInfo).moduleCodeOffset = optionalHeader.BaseOfCode;
-
-	printf("# sections=%d\n", (*peHeader).FileHeader.NumberOfSections);
-	TraverseSectionHeaders
-	(
-		IMAGE_FIRST_SECTION(peHeader),
-		(*peHeader).FileHeader.NumberOfSections,
-		addrInfo
-	);
-	return;
-}
-
-void TraverseSectionHeaders
-(
-	PIMAGE_SECTION_HEADER section,
-	DWORD nSections,
-	PADDRESS_INFO addrInfo
-)
+void TraverseSectionHeaders(PIMAGE_SECTION_HEADER section, DWORD nSections, PADDRESS_INFO addrInfo)
 {
 	DWORD i;
 	printf("[Dump sections]:------------------------------\n\n");
@@ -53,8 +25,26 @@ void TraverseSectionHeaders
 	return;
 }
 
-int getHMODULE(const char* fileName, DWORD* hFileMapping)
-{	
+void GetCodeLoc(DWORD* baseAddress, PADDRESS_INFO addrInfo)
+{
+	PIMAGE_DOS_HEADER dosHeader;
+	PIMAGE_NT_HEADERS peHeader;
+	IMAGE_OPTIONAL_HEADER32 optionalHeader;
+
+	dosHeader = (PIMAGE_DOS_HEADER)baseAddress;
+	peHeader = (PIMAGE_NT_HEADERS)((DWORD)baseAddress + (*dosHeader).e_lfanew);
+	optionalHeader = (*peHeader).OptionalHeader;
+
+	(*addrInfo).moduleBase = optionalHeader.ImageBase;
+	(*addrInfo).moduleCodeOffset = optionalHeader.BaseOfCode;
+
+	printf("# sections=%d\n", (*peHeader).FileHeader.NumberOfSections);
+	TraverseSectionHeaders(IMAGE_FIRST_SECTION(peHeader), (*peHeader).FileHeader.NumberOfSections, addrInfo);
+	return;
+}
+
+int getHMODULE(const char* fileName, DWORD** hFileMapping)
+{
 	HANDLE hFile = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
@@ -62,29 +52,35 @@ int getHMODULE(const char* fileName, DWORD* hFileMapping)
 		return 0;
 	}
 	DWORD fileSize = GetFileSize(hFile, 0);
-	BYTE* hFileMapping = (BYTE*)GlobalAlloc(GMEM_FIXED | GMEM_ZEROINIT, fileSize);
+	*hFileMapping = (DWORD*)GlobalAlloc(GMEM_FIXED | GMEM_ZEROINIT, fileSize);
 	DWORD tmp;
-	ReadFile(hFile, hFileMapping, fileSize, &tmp, 0);
+	ReadFile(hFile, *hFileMapping, fileSize, &tmp, 0);
 	CloseHandle(hFile);
 	return 1;
 }
 
-void cipherBytes(const char* fileName)
+void cipherBytes(DWORD* hInMem, PADDRESS_INFO addrInfo)
 {
-
+	BYTE* codeStart = (BYTE*)(hInMem + (DWORD)(*addrInfo).fileCodeOffset);
+	int curPos = 0;
+	for(int i = 0; i < (*addrInfo).fileCodeSize; i++)
+	{
+		*codeStart = *codeStart ^ 0x0f;
+		codeStart++;
+	}
 }
 
 int main()
 {
 	DWORD retVal;
-	DWORD hFileMapping;
+	DWORD* hFileMapping = 0;
 	ADDRESS_INFO addrInfo;
-	
-	char fileName[] = "filename";
-	retVal = getHMODULE(fileName, &hFileMapping);
-	if (retVal == FALSE) { return; }
 
-	GetCodeLoc(&hFileMapping, &addrInfo);
-	cipherBytes(fileName, &addrInfo);
+	char fileName[] = "C:\\Users\\atsee\\source\\repos\\CodeEncryption\\Debug\\CodeEncryption.exe";
+	retVal = getHMODULE(fileName, &hFileMapping);
+	if (retVal == FALSE) { return -1; }
+
+	GetCodeLoc(hFileMapping, &addrInfo);
+	cipherBytes(hFileMapping, &addrInfo);
 	return 0;
 }
